c00f5544c95ef2b78c10cc7343a053004d36ae66
Ignacio Palma <ignacio.palma.@live.com>
2016-05-16 16:31:24 -0300

    añadiendo archivos faltantes

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,51 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @comments = Comment.all
+  end
+
+  def show
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def edit
+  end
+
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+
+    redirect_to post_path(@post)
+   
+  end
+
+
+  def update
+      if @comment.update(comment_params)
+        redirect_to @comment, notice: 'Comment was successfully updated.' 
+      else
+        render :edit 
+      end
+  end
+
+
+  def destroy
+    @comment.destroy
+      redirect_to comments_url, notice: 'Comment was successfully destroyed.' 
+    end
+  end
+
+  private
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:name, :description, :post_id)
+    end
+end
